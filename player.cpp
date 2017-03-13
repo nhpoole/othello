f52fff81a91fc7a7aad82d43d21c2d7fa8b32c8b
nhpoole <nhpoole@caltech.edu>
2017-03-13 00:58:10 -0700

    alpha beta working properly

--- player.cpp
+++ player.cpp
@@ -37,6 +37,7 @@ Player::Player(Side side) {
 Player::~Player() {
 }
 
+
 /*
  * Compute the next move given the opponent's last move. Your AI is
  * expected to keep track of the board on its own. If this is the first move,
@@ -50,97 +51,6 @@ Player::~Player() {
  * The move returned must be legal; if there are no valid moves for your side,
  * return nullptr.
  */
-Move * Player::doAlphaBeta(Board *board, int depth, Side player)
-{
-    double max  = INT_MIN;
-    int bestx;
-    Side otherPlayer = player;
-    if(player == playerSide)
-    {
-        otherPlayer = opponentSide;
-    }
-    if(player == opponentSide)
-    {
-        otherPlayer = playerSide;
-    }
-    for(int x = 0;x<othelloBoard->possibleMoves(player).size();x++)
-    {
-        Board *othelloBoardCopy = board->copy();
-        board->doMove(othelloBoardCopy->possibleMoves(player)[x], player);
-        int z = alphaBeta(othelloBoardCopy, depth, INT_MIN, INT_MAX, true, otherPlayer);
-        delete othelloBoardCopy;
-        if(z > max)
-        {
-            max = z;
-            bestx = x;
-        }
-    }
-    return othelloBoard->possibleMoves(player)[bestx];
-}
-double Player::alphaBeta(Board *board, int depth, double alpha, double beta, bool maximizing, Side player)
-{
-    if(depth ==0 || board->possibleMoves(player).size() ==0)
-    {
-        return (double)board->getDiffScore(player);
-    }
-    Side otherPlayer = player;
-    if(player == playerSide)
-    {
-        otherPlayer = opponentSide;
-    }
-    if(player == opponentSide)
-    {
-        otherPlayer = playerSide;
-    }
-    if (maximizing)
-    {
-        double v = INT_MIN;
-        for(int x = 0;x<board->possibleMoves(player).size();x++)
-        {
-            Board *othelloBoardCopy = board->copy();
-            othelloBoardCopy->doMove(board->possibleMoves(player)[x], player);
-            double alphaBetaCurrent = alphaBeta(othelloBoardCopy, depth-1, alpha, beta, true, otherPlayer);
-            delete othelloBoardCopy;
-            if(v<alphaBetaCurrent)
-            {
-                v = alphaBetaCurrent;
-            }
-            if(alpha < v)
-            {
-                alpha = v;
-            }
-            if(beta <= alpha)
-            {
-                break;
-            }
-        }
-        return v;
-    }
-    else
-    {
-        double v = INT_MAX;
-        for(int x = 0;x<board->possibleMoves(player).size();x++)
-        {
-            Board *othelloBoardCopy = board->copy();
-            othelloBoardCopy->doMove(board->possibleMoves(player)[x], player);
-            double alphaBetaCurrent = alphaBeta(othelloBoardCopy, depth-1, alpha, beta, false, otherPlayer);
-            delete othelloBoardCopy;
-            if(v > alphaBetaCurrent)
-            {
-                v = alphaBetaCurrent;
-            }
-            if(beta > v)
-            {
-                beta = v;
-            }
-            if(beta<=alpha)
-            {
-                break;
-            }
-        }
-        return v;
-    }
-}
 Move *Player::doMove(Move *opponentsMove, int msLeft) {
     /*
      * TODO: Implement how moves your AI should play here. You should first
@@ -155,98 +65,39 @@ Move *Player::doMove(Move *opponentsMove, int msLeft) {
         
     if (othelloBoard->hasMoves(playerSide))
     {
-
-        // vector<Move*> possible_moves = 
-        //         othelloBoard->possibleMoves(playerSide);
-                           
-        // random move player
-        /*  int rand_index = rand() % possible_moves.size();
-        cerr << possible_moves.size() << endl;
-        Move *rand_move = new Move(possible_moves[rand_index]->x,
-                                  possible_moves[rand_index]->y);*/
-        
-        
-        
-        // heuristic move player
-        //
-        // int max_score =  -100000;
-        // int temp_score;
-        // Move *best_move = possible_moves[0];        
-        // for (unsigned int i = 0; i < possible_moves.size(); i++)
-        // {
-        //     Board *board_copy = othelloBoard->copy();
-        //     board_copy->doMove(possible_moves[i], playerSide);
-        //     temp_score = board_copy->calculateScore(playerSide);
-        //     if (max_score < temp_score)
-        //     {
-        //         max_score = temp_score;
-        //         cerr << "got here" << endl;
-        //         best_move->setX(possible_moves[i]->x);
-        //         best_move->setY(possible_moves[i]->y);
-        //     }
-        //     delete board_copy;
-        // }
-        
-        
-        
-        // minimax algorithm
-       // int diff_score = othelloBoard->getDiffScore(playerSide);
-        
-        
-        
-        
-
-    //    othelloBoard->white_score = othelloBoard->calculateScore(WHITE);
-    //    othelloBoard->black_score = othelloBoard->calculateScore(BLACK);
-                    
-        // random move player
-     //   othelloBoard->doMove(rand_move, playerSide);
-     //   cerr << "recorded move" << endl;
-     //   return rand_move;
-        
-        // heuristic move player
-        // othelloBoard->doMove(best_move, playerSide);
-        // return best_move;
-
-
-        //minimax
-        // int maximum_min = INT_MIN;
-        // int bestX = 0;
-        
-        // for (unsigned int x = 0; 
-        //      x < othelloBoard->possibleMoves(playerSide).size(); x++)
-        // {
-        //     Board *newOthelloCopy = othelloBoard->copy();
-        //     newOthelloCopy->doMove(newOthelloCopy->possibleMoves(playerSide)[x],playerSide);
-        //     int localMin = INT_MAX;
-        //     for (unsigned int y = 0; y < newOthelloCopy->possibleMoves(opponentSide).size(); y++)
-        //     {
-        //         Board *newNewOthelloCopy = newOthelloCopy->copy();
-        //         newNewOthelloCopy->doMove(newNewOthelloCopy->possibleMoves(opponentSide)[y], opponentSide);
-               
-        //         if (newNewOthelloCopy->getDiffScore(playerSide) < localMin)
-        //         {
-        //             localMin = newNewOthelloCopy->getDiffScore(playerSide);
-        //         }
-               
-        //       // For better heuristic
-        //        /* if (newNewOthelloCopy->calculateScore(playerSide) < localMin)
-        //         {
-        //             localMin = newNewOthelloCopy->calculateScore(playerSide);
-        //         } */
-        //         delete newNewOthelloCopy;
-        //     }
-        //     if (localMin > maximum_min)
-        //     {
-        //         maximum_min = localMin;
-        //         bestX = x;
-        //     }
-        //     delete newOthelloCopy;
-        // }
+         vector<Move*> possible_moves = 
+                 othelloBoard->possibleMoves(playerSide);
         
         // Move *bestMove = othelloBoard->possibleMoves(playerSide)[bestX];
-        Move *bestMove = doAlphaBeta(othelloBoard, 10, playerSide);
-        std::cerr<<"move";
+       /* Move *bestMove = doAlphaBeta(othelloBoard, 10, playerSide);
+        std::cerr<<"move";*/
+        
+        int bestX;
+        Move *bestMove = possible_moves[0];
+       // cerr << "possible moves: \n";
+       /* for (unsigned int i = 0; i < possible_moves.size(); i++)
+        {
+            cerr << "move: (" << possible_moves[i]->x << ", " << possible_moves[i]->y << ")\n";
+        }    
+        cerr << endl;   */     
+        
+        // 2-ply minimax
+       /* bestX = two_ply_minimax(possible_moves);
+        bestMove = possible_moves[bestX];*/
+        
+        // recursive minimax
+       /* recursive_minimax(MINIMAX_DEPTH - 1, playerSide, possible_moves,
+                                 othelloBoard, bestMove);*/
+                                  
+        // alpha-beta pruning
+        int alpha = INT_MIN;
+        int beta = INT_MAX;       
+        
+        alpha_beta_prune(MINIMAX_DEPTH - 1, playerSide, possible_moves,
+                         othelloBoard, bestMove, alpha, beta);
+        
+       // cerr << "best move: (" << bestMove->x << ", " << bestMove->y << ")\n\n";        
+        
         othelloBoard->doMove(bestMove, playerSide);
         return bestMove;
     }
@@ -256,3 +107,231 @@ Move *Player::doMove(Move *opponentsMove, int msLeft) {
         return nullptr;
     }
 }
+
+
+// return the index of the best move in the possible moves for two ply
+int Player::two_ply_minimax(vector<Move*> possible_moves)
+{
+    int maximum_min = INT_MIN;
+    int bestX = 0;
+    
+    for (unsigned int x = 0; x < possible_moves.size(); x++)
+    {
+        Board *newOthelloCopy = othelloBoard->copy();
+        newOthelloCopy->doMove(possible_moves[x], playerSide);
+        int localMin = INT_MAX;
+        vector<Move*> opponent_possibles = newOthelloCopy->
+                                           possibleMoves(opponentSide);
+        for (unsigned int y = 0; y < opponent_possibles.size(); y++)
+        {
+            Board *newNewOthelloCopy = newOthelloCopy->copy();
+            newNewOthelloCopy->doMove(opponent_possibles[y], opponentSide);
+           
+            // better heuristic
+            if (testingMinimax == false)
+            {
+                if (newNewOthelloCopy->calculateScore(playerSide) < localMin)
+                {
+                    localMin = newNewOthelloCopy->calculateScore(playerSide);
+                }
+            }
+            
+            // simple difference heuristic               
+            else
+            {
+                if (newNewOthelloCopy->getDiffScore(playerSide) < localMin)
+                {
+                    localMin = newNewOthelloCopy->getDiffScore(playerSide);
+                }
+            }          
+            delete newNewOthelloCopy;
+        }
+        
+        if (localMin > maximum_min)
+        {
+            maximum_min = localMin;
+            bestX = x;
+        }
+        delete newOthelloCopy;
+    }
+    
+    return bestX;
+}    
+
+int Player::recursive_minimax(int depth, Side side, vector<Move*> possible_moves,
+                              Board *new_board, Move *&best_move)
+{    
+    int depth_counter = MINIMAX_DEPTH - depth;
+    int best_score;
+    int local_score;
+    
+    if (side == opponentSide)
+    {
+        best_score = INT_MAX;
+    }
+    else
+    {
+        best_score = INT_MIN;
+    }
+
+    for (unsigned int x = 0; x < possible_moves.size(); x++)
+    {
+        Board *newOthelloCopy = new_board->copy();
+        newOthelloCopy->doMove(possible_moves[x], side);
+        if (depth_counter < MINIMAX_DEPTH)
+        { 
+            vector<Move*> next_possibles;
+            Side new_side;
+            if (side == playerSide)
+            {
+                next_possibles = newOthelloCopy->possibleMoves(opponentSide);
+                new_side = opponentSide;
+            }
+            else
+            {
+                next_possibles = newOthelloCopy->possibleMoves(playerSide);
+                new_side = playerSide;
+            }
+            
+            local_score = recursive_minimax(depth - 1, new_side, next_possibles,
+                                            newOthelloCopy, best_move);
+            for (unsigned int i = 0; i < next_possibles.size(); i++)
+            {
+                delete next_possibles[i];
+            }
+        }
+        
+        else
+        {
+            if (testingMinimax == false)
+            {
+                local_score = newOthelloCopy->calculateScore(playerSide);
+            }
+            else
+            {
+                local_score = newOthelloCopy->getDiffScore(playerSide);
+            }                
+        }
+        
+        if (side == opponentSide)
+        {
+            if (local_score < best_score)
+            {
+                best_score = local_score;
+                if (depth_counter == 1)
+                {
+                    best_move = possible_moves[x];
+                }
+            }
+        }
+        else
+        {
+            if (local_score > best_score)
+            {
+                best_score = local_score;
+                if (depth_counter == 1)
+                {
+                    best_move = possible_moves[x];
+                }
+            }
+        }
+
+        delete newOthelloCopy;
+    }
+    
+    return best_score;
+}
+
+int Player::alpha_beta_prune(int depth, Side side, vector<Move*> possible_moves,
+                             Board *new_board, Move *&best_move, int Alpha, 
+                             int Beta)
+{    
+    int depth_counter = MINIMAX_DEPTH - depth;
+    int local_score;
+    int alpha = Alpha;
+    int beta = Beta;
+    
+
+    for (unsigned int x = 0; x < possible_moves.size(); x++)
+    {
+        Board *newOthelloCopy = new_board->copy();
+        newOthelloCopy->doMove(possible_moves[x], side);
+        if (depth_counter < MINIMAX_DEPTH)
+        { 
+            vector<Move*> next_possibles;
+            Side new_side;
+            if (side == playerSide)
+            {
+                next_possibles = newOthelloCopy->possibleMoves(opponentSide);
+                new_side = opponentSide;
+            }
+            else
+            {
+                next_possibles = newOthelloCopy->possibleMoves(playerSide);
+                new_side = playerSide;
+            }
+            
+            local_score = alpha_beta_prune(depth - 1, new_side, next_possibles,
+                                        newOthelloCopy, best_move, alpha, beta);
+            for (unsigned int i = 0; i < next_possibles.size(); i++)
+            {
+                delete next_possibles[i];
+            }
+        }
+        
+        else
+        {
+            if (testingMinimax == false)
+            {
+                local_score = newOthelloCopy->calculateScore(playerSide);
+            }
+            else
+            {
+                local_score = newOthelloCopy->getDiffScore(playerSide);
+            }                
+        }
+        
+        if (side == opponentSide)
+        {
+            if (local_score < beta)
+            {
+                beta = local_score;
+                if (depth_counter == 1)
+                {
+                    best_move = possible_moves[x];
+                }
+            }
+            if (beta <= alpha)
+            {
+                break;
+            }
+        }
+        else
+        {
+            if (local_score > alpha)
+            {
+                alpha = local_score;
+                if (depth_counter == 1)
+                {
+                    best_move = possible_moves[x];
+                }
+            }
+            if (alpha >= beta)
+            {
+                break;
+            }
+        }
+
+        delete newOthelloCopy;
+    }
+    
+    
+    if (side == opponentSide)
+    {
+        return beta;
+    }
+    else
+    {
+        return alpha;
+    }
+}    
--- player.hpp
+++ player.hpp
@@ -1,5 +1,6 @@
 #ifndef __PLAYER_H__
 #define __PLAYER_H__
+#define MINIMAX_DEPTH 7
 
 #include <iostream>
 #include "common.hpp"
@@ -12,10 +13,15 @@ public:
     Player(Side side);
     ~Player();
 
-    Move *doMove(Move *opponentsMove, int msLeft);
-    double alphaBeta(Board *board, int depth, double alpha, double beta, bool maximizing, Side player);
+    Move *doMove(Move *opponentsMove, int msLeft);  
+    
+    int two_ply_minimax(vector<Move*> possible_moves);
+    int recursive_minimax(int depth, Side side, vector<Move*> possible_moves,
+                          Board *new_board, Move *&best_move);
+    int alpha_beta_prune(int depth, Side side, vector<Move*> possible_moves,
+                             Board *new_board, Move *&best_move, int alpha, 
+                             int beta);   
     // Flag to tell if the player is running within the test_minimax context
-    Move * doAlphaBeta(Board *board, int depth, Side player);
     bool testingMinimax;
     Side playerSide;
     Side opponentSide;
